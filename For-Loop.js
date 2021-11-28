256153574d50d0f8e658d749046196c086246dda
Piyush <94220242+Piyush-Meena@users.noreply.github.com>
2021-11-28 06:33:52 +0530

    For-Loop.js

---
+++ For-Loop.js
@@ -0,0 +1,9 @@
+// initialize j variable
+let j = 1;
+for (;;) {
+    // terminate the loop if j is greater than 10;
+    if (j > 10) break;
+    console.log(j);
+    // increase the counter j
+    j += 2;
+}
